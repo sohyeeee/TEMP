2e60eec623dff75d93b7385ba6580de80c31816b
도소혜 <sohye120@gamil.com>
2023-03-19 21:59:08 +0900

    자바

--- Hi/src/J5_5.java
+++ Hi/src/J5_5.java
@@ -5,7 +5,7 @@ public class J5_5 {
 	public static void main(String[] args) {
 				int[] ball = new int[45];  // 45개의 정수값을 저장하기 위한 배열 생성   
 
-				// 배열의 각 요소에 1~45값을 저장한다. 
+				// 배열의 각 요소에 1~45값을 저장한다
 				for(int i=0; i < ball.length; i++)       
 					ball[i] = i+1;    // ball[0]에 1이 저장된다.
 				System.out.println(Arrays.toString(ball));
