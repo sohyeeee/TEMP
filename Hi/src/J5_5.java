4d4a6acec061d69ee3ce3bfe012ee16e65c51e41
도소혜 <sohye120@gamil.com>
2023-03-20 14:30:10 +0900

    예제 다시 연습

--- Hi/src/J5_5.java
+++ Hi/src/J5_5.java
@@ -3,24 +3,22 @@ import java.util.Arrays;
 public class J5_5 {
 
 	public static void main(String[] args) {
-				int[] ball = new int[45];  // 45개의 정수값을 저장하기 위한 배열 생성   
-
-				// 배열의 각 요소에 1~45값을 저장한다
-				for(int i=0; i < ball.length; i++)       
-					ball[i] = i+1;    // ball[0]에 1이 저장된다.
-				System.out.println(Arrays.toString(ball));
-
-				int tmp = 0;   
-				int j = 0;     
-
-				for(int i=0; i < 6; i++) {       
-					j = (int)(Math.random() * 45); 
-					tmp     = ball[i]; 
-					ball[i] = ball[j]; 
-					ball[j] = tmp; 
-				} 
-
-				for(int i=0; i < 6; i++) 
-					System.out.printf("ball[%d]=%d%n", i, ball[i]); 
+		int[] ball = new int[45];
+		
+		for(int i=0;i<ball.length;i++) {
+			ball[i] = i+1;
+		}
+		int tmp, j = 0;
+		
+		for(int i=0; i<5; i++) {
+			j = (int)(Math.random()*45);
+			tmp = ball[i];
+			ball[i] = ball[j];
+			ball[j] = tmp;
+//			System.out.println(Arrays.toString(ball));
+		}
+		for(int i=0;i<6;i++) {
+			System.out.printf("ball[%d]=%d%n",i,ball[i]);
+		}
 	}
 }
